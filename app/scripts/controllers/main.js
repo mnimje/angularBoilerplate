00ceb3be260e9fbfbbd6fab89820a11b5f7e8394
mnimje <mayanknimje87@gmail.com>
2016-09-25 19:09:33 +0530

    initial commit 25oct16

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('angularMApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
