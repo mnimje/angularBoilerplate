68f938b0f3d02052feaeff53d94387e665e8a7fb
mnimje <mayanknimje87@gmail.com>
2016-10-10 14:32:48 +0530

    merging new files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,5 @@
 'use strict';
 angular.module('angularMApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
